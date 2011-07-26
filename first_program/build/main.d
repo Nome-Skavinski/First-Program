6aa942e04c25d78e35c59ee8cce62768d8a359ce
Nome Skavinski <da_nome666@hotmail.com>
2011-07-26 14:31:27 +0100

    first commit

---
+++ first_program/build/main.d
@@ -0,0 +1,34 @@
+main.o: /home/nome/ps3dev/projects/first_program/source/main.c \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/io/pad.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/ppu-types.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/msg.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/sysutil.h \
+ /home/nome/ps3dev/projects/first_program/include/rsxutil.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/mm.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/gcm_sys.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx_program.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/commands.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/video.h
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/io/pad.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/ppu-types.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/msg.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/sysutil.h:
+
+/home/nome/ps3dev/projects/first_program/include/rsxutil.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/mm.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/gcm_sys.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx_program.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/commands.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/video.h:
---
+++ first_program/build/rsxutil.d
@@ -0,0 +1,25 @@
+rsxutil.o: /home/nome/ps3dev/projects/first_program/source/rsxutil.c \
+ /home/nome/ps3dev/projects/first_program/include/rsxutil.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/mm.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/ppu-types.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/gcm_sys.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx_program.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/rsx/commands.h \
+ /usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/video.h
+
+/home/nome/ps3dev/projects/first_program/include/rsxutil.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/mm.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/ppu-types.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/gcm_sys.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/rsx_program.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/rsx/commands.h:
+
+/usr/local/ps3dev/libpsl1ght/ppu/include/sysutil/video.h:
